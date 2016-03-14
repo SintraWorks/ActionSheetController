c2a78799773fe4008215422dbcd9853e32cd279e
Antonio Nunes <antonio.nunes@sintraworks.com>
2016-03-14 12:28:13 +0100

    Initial Commit

---
+++ ActionSheetController/ActionSheetController.h
@@ -0,0 +1,19 @@
+//
+//  ActionSheetController.h
+//  ActionSheetController
+//
+//  Created by Antonio Nunes on 14/03/16.
+//  Copyright © 2016 SintraWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActionSheetController.
+FOUNDATION_EXPORT double ActionSheetControllerVersionNumber;
+
+//! Project version string for ActionSheetController.
+FOUNDATION_EXPORT const unsigned char ActionSheetControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActionSheetController/PublicHeader.h>
+
+
